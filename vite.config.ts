f153f1b81f2dee908aa725e3f157ba4848b1880f
Hightouch <cetinkaya.ozden@gmail.com>
2025-08-12 16:49:20 +0200

    update environment variable

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,35 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    tailwindcss(),
-    Pages({
-      extensions: ['vue', 'tsx', 'ts'],
-    }),
-    Components({
-      dirs: ['src/components'],
-      deep: true,
-      dts: 'src/components.d.ts',
-    }),
-    AutoImport({
-      imports: [
-        'vue',
-        'vue-router',
-      ],
-      dts: 'src/auto-imports.d.ts',
-      vueTemplate: true,
-    }),
-  ],
-})
+export default ({ mode }: { mode: string }) => {
+  const env = loadEnv(mode, process.cwd())
+  return defineConfig({
+    server: {
+      port: Number(env.VITE_PORT) || 3000
+    },
+    plugins: [
+      vue(),
+      tailwindcss(),
+      Pages({
+        extensions: ['vue', 'tsx', 'ts'],
+      }),
+      Components({
+        dirs: ['src/components'],
+        deep: true,
+        dts: 'src/components.d.ts',
+      }),
+      AutoImport({
+        imports: [
+          'vue',
+          'vue-router',
+        ],
+        dts: 'src/auto-imports.d.ts',
+        vueTemplate: true,
+      }),
+    ],
+  })
+}
